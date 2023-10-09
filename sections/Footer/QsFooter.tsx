2b80e6ed83d84296c44ca923392c37b3e218becc
RaviHelon-msf <ravihelon@gmail.com>
2023-10-09 15:40:37 -0300

    Feat: add lowerthird of footer
    A parte inferior do footer como está no site do cliente, para facilitar a transição

--- sections/Footer/QsFooter.tsx
+++ sections/Footer/QsFooter.tsx
@@ -1,27 +1,47 @@
 
 export function QsFooter() {
-    return (
-      <>
-        <div class="h-1000 md:h-500 flex w-full flex-col gap-10 bg-[#181818] px-[100px] py-[50px] text-white md:flex-row">
-          <div class="block">
-            <img src="https://www.quiksilver.com.br/Custom/Content/Themes/Quiksilver/Images/shield.png" />
-            <h3 class="py-3 text-2xl font-bold">Protect our Playgrounds</h3>
-            <p>Porque, sem eles, não seríamos os mesmos. Cabe a nós garantir que eles estejam por perto para as gerações futuras desfrutarem.</p>
+  return (
+    <>
+      <div class="flex h-[500] w-full flex-col bg-[#181818] text-[#ffffff]">
+
+
+
+
+
+        <div class="flex flex-col items-center border-t-2 border-gray-600">
+          <div class="">
+            <a href="http:global.quiksilver.com?geo=none" class="flex items-center justify-center gap-2 p-7 text-sm font-semibold" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+              </svg>
+              <p class="text-center">Escolha sua região</p>
+            </a>
           </div>
-          <div class="bloco-2">
-            <img src="https://www.quiksilver.com.br/Custom/Content/Themes/Quiksilver/Images/hand.png" />
-            <h3 class="py-3 text-2xl font-bold">Respect our People</h3>
-            <p>Nossa onda é para todos. Promovemos uma cultura que apoia a diversidade, a autoexpressão e as escolhas sustentáveis.</p>
+          <div class="m-0 flex h-[55px] w-[55px] justify-center items-center">
+            <a href="https://www.quiksilver.com.br/" class="">
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Calque_1" class="h-[50px] w-[50px] transition-all hover:h-[55px] hover:w-[55px]" viewBox="0 0 734.2 388.4">
+                <g>
+                  <path fill="white" d="M734.2,388.3H40.9c-23.8,0-40.6-20-40.9-39.1V193.8c29.8,0,56.6-14.3,66.3-21.3c61.8-44.2,113.2-130.3,179-158.3   C264.6,6,302.1-7.3,349.3,5l56.5,56.3c0,0-29.5-13.9-73.3-13.9c-49.1,0-143.9,40.5-143.9,146.3c0,85.2,58.7,145.1,155.1,145.1   h340.9L734.2,388.3z" />
+                  <path fill="white" d="M338.3,196.3c-3.6,3.5-1.7,7.6,3,7.5h39.1l27.5-23.3c5.8-5.7,11.3-4.6,11.3,1.5v46.4c-0.1,7.4,6.4,7.4,10.3,2.4l28.6-27   h37.5c3-0.1,5.1-3.9,1.1-7.8l-67.7-66.6c-4.8-4.9-15.3-4.9-19.9,0C407.5,131,338.3,196.3,338.3,196.3 M654.2,308.7H336.4   c-36,0-84.2-17.2-98.8-50.7c0,0,142.9-145.8,149.8-152.7c17.1-17,47.6-17,62.4,0L654.2,308.7z" />
+                </g>
+              </svg>
+            </a>
           </div>
-          <div class="bloco-3">
-            <img src="https://www.quiksilver.com.br/Custom/Content/Themes/Quiksilver/Images/sea.png" />
-            <h3 class="py-3 text-2xl font-bold">Reduce our Impact</h3>
-            <p>Defender a sustentabilidade e impulsionar a mudança por meio da transparência e responsabilidade, um passo de cada vez.</p>
+
+          <div class="mx-5 mb-6 mt-3 flex flex-col items-center justify-center text-sm font-light">
+            <div class="flex items-center justify-center text-center">
+              <a href="/conteudo/atendimento-consumidor-quiksilver/politica-privacidade-quiksilver" title="Política de Privacidade" target="" class="border-r border-r-gray-500 pr-1 hover:underline">Política de Privacidade </a>
+
+              <a href="/conteudo/atendimento-consumidor-quiksilver/termos-e-condicoes-quiksilver" title="Termos e Condições" target="" class="pl-1 hover:underline"> Termos e Condições</a>
+            </div>
+            <div class="justify-center text-center">
+              <p class="">Copyright © Quiksilver 2023, Todos os direitos reservados.</p>
+            </div>
           </div>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+  </>
+  );
+}
+
 export default QsFooter;
-  
